9bba6d3a403b7befbe24e333e9d64119a7d5a694
Yulia Samoykina <yuliasam89@gmail.com>
2021-05-10 11:35:36 +0300

    If_learn

--- Javascript/Learning.js
+++ Javascript/Learning.js
@@ -1,15 +1,6 @@
 "use strict"
-let age = prompt ("Когда вы родились?", 18);
-
-if (age < 3) {
-    message = "Hi";
-} else if (age < 18) {
-    message = "How are you?";
-} else if (age < 100) {
-    message = "Good age";
-} else {
-    message = "Old enough";
-}
-
+let login = prompt ("Your position?", "");
+let message = (login == "Сотрудник") ? "Hi" :
+(login == "Директор") ? "Hello" :
+(login == " ") ? message = "No login" : " ";
 alert(message);
-
